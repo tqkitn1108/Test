56e799145c166b1c9b7e85649297ed0f68c7db4f
Sakary03 <107931611+Sakary03@users.noreply.github.com>
2024-10-28 22:23:53 +0700

    update FeartureHome Component

--- src/customer/pages/home/explore/Explore.jsx
+++ src/customer/pages/home/explore/Explore.jsx
@@ -9,44 +9,33 @@ const Explore = () => {
     const slidesPerView = 4;
 
     return (
-        <div className="flex justify-center w-full py-8">
-            <div className="w-full max-w-[1200px] relative">
-                <Swiper
-                    navigation={{
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
-                    }}
-                    modules={[Navigation]}
-                    slidesPerView={slidesPerView}
-                    spaceBetween={16}
-                    loop={true}
-                    className="swiper-container"
-                >
-                    {destinations.map((destinations, i) => (
-                        <SwiperSlide key={i} className="swiper-slide">
-                            <div className="overflow-hidden cursor-pointer flex flex-col items-center">
-                                <img
-                                    src={destinations.image}
-                                    alt={destinations.label}
-                                    className="rounded-lg w-full h-[140px] object-cover md:h-[130px]"
-                                    style={{ height: '140px' }}
-                                />
-                                <div className="text-center mt-2">
-                                    <h5 className="text-lg">{destinations.name}</h5>
-                                    <h5 className="text-gray-700 font-normal mt-1 text-sm">0 chỗ nghỉ</h5>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                    <div className="absolute top-1/2 left-0 transform -translate-y-1/2 swiper-button-prev">
-                        <IoCaretBackCircleOutline className="w-10 h-10 text-indigo-600 cursor-pointer" />
+        {
+            destinations.map((destinations, i) => (
+                <SwiperSlide key={i} className="swiper-slide">
+                    <div className="overflow-hidden cursor-pointer flex flex-col items-center">
+                        <img
+                            src={destinations.image}
+                            alt={destinations.label}
+                            className="rounded-lg w-full h-[140px] object-cover md:h-[130px]"
+                            style={{ height: '140px' }}
+                        />
+                        <div className="text-center mt-2">
+                            <h5 className="text-lg">{destinations.name}</h5>
+                            <h5 className="text-gray-700 font-normal mt-1 text-sm">0 chỗ nghỉ</h5>
+                        </div>
                     </div>
-                    <div className="absolute top-1/2 right-0 transform -translate-y-1/2 swiper-button-next">
-                        <IoCaretForwardCircleSharp className="w-10 h-10 text-indigo-600 cursor-pointer" />
-                    </div>
-                </Swiper>
-            </div>
-        </div>
+                </SwiperSlide>
+            ))
+        }
+        < div className = "absolute top-1/2 left-0 transform -translate-y-1/2 swiper-button-prev" >
+            <IoCaretBackCircleOutline className="w-10 h-10 text-indigo-600 cursor-pointer" />
+                    </div >
+    <div className="absolute top-1/2 right-0 transform -translate-y-1/2 swiper-button-next">
+        <IoCaretForwardCircleSharp className="w-10 h-10 text-indigo-600 cursor-pointer" />
+    </div>
+                </Swiper >
+            </div >
+        </div >
     );
 };
 

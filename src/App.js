f687cde942f311ce96daf10b0fda5b96ee2374ee
Nguyễn Đức Khoa <khoavkvk@gmail.com>
2024-10-29 16:13:02 +0700

    Create login page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Navbar from "./customer/navbar/Navbar.jsx";
 import Header from "./customer/header/Header.jsx";
 import SearchItem from "./customer/page/list/SearchItem.jsx";
 import List from "./customer/page/list/List.jsx";
+import Login from "./customer/page/login-register/Login.jsx";
 
 export default function App() {
   return (
     <div className="App">
-      <List />
+      <Login />
     </div>
   );
 }

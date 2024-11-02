380ad2f3a0ec004e736de0d8e2b3c33c3b8f7e83
Khuong <khuong29102003@gmail.com>
2024-11-03 00:44:07 +0700

    update hotel reservation

--- src/customer/pages/hotel/SearchBar.jsx
+++ src/customer/pages/hotel/SearchBar.jsx
@@ -15,7 +15,6 @@ function SearchBar({ setShowRooms }) {
   const searchParams = new URLSearchParams(location.search);
   const [defaultText, setDefaultText] = useState(true);
   const navigate = useNavigate();
-
   const handleSearch = (event) => {
     event.preventDefault();
     if (defaultText) {
@@ -32,7 +31,6 @@ function SearchBar({ setShowRooms }) {
       setShowRooms(true);
     }
   };
-
   useEffect(() => {
     if (searchParams.get('checkIn')) {
       setDefaultText(false);
@@ -46,23 +44,23 @@ function SearchBar({ setShowRooms }) {
       adult: parseInt(searchParams.get('adults')) || 2,
       children: parseInt(searchParams.get('children')) || 0,
       room: parseInt(searchParams.get('noRooms')) || 1,
-    });
-  }, []);
+    })
+  }, [])
 
-  const [openDate, setOpenDate] = useState(false);
-  const [date, setDate] = useState([
-    {
-      startDate: new Date(),
-      endDate: new Date(),
-      key: 'selection'
-    }
-  ]);
-
+  const [openDate, setOpenDate] = useState(false)
+  const [date, setDate] = useState(
+    [
+      {
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection'
+      }
+    ]
+  );
   const handleDateChange = (item) => {
     setDate([item.selection]);
     setDefaultText(false);
   };
-
   const componentRef = useRef(null);
   const componentRef2 = useRef(null);
 
@@ -80,6 +78,7 @@ function SearchBar({ setShowRooms }) {
       document.removeEventListener('mousedown', handleOutsideClick);
     };
   }, []);
+
 
   const [openOptions, setOpenOptions] = useState(false);
   const [options, setOptions] = useState({
@@ -104,7 +103,7 @@ function SearchBar({ setShowRooms }) {
         <div className="header-search-item" onClick={() => setOpenDate(!openDate)}>
           <div className="header-search-text" >
             <span className="header-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22.5 13.5v8.25a.75.75 0 0 1-.75.75H2.25a.75.75 0 0 1-.75-.75V5.25a.75.75 0 0 1 .75-.75h19.5a.75.75 0 0 1 .75.75v8.25zm1.5 0V5.25A2.25 2.25 0 0 0 21.75 3H2.25A2.25 2.25 0 0 0 0 5.25v16.5A2.25 2.25 0 0 0 2.25 24h19.5A2.25 2.25 0 0 0 24 21.75V13.5zm-23.25-3h22.5a.75.75 0 0 0 0-1.5H.75a.75.75 0 0 0 0 1.5zM7.5 6V.75a.75.75 0 0 0-1.5 0V6a.75.75 0 0 0 1.5 0zM18 6V.75a.75.75 0 0 0-1.5 0V6A.75.75 0 0 0 18 6zM5.095 14.03a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zm.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5zm-.53 6.53a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zm.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5zm5.845-3.97a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zm.53-1.28A1.125 1.125 0 1 0 12 15a1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5zm-.53 6.53a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zm.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5z"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22.5 13.5v8.25a.75.75 0 0 1-.75.75H2.25a.75.75 0 0 1-.75-.75V5.25a.75.75 0 0 1 .75-.75h19.5a.75.75 0 0 1 .75.75v8.25zm1.5 0V5.25A2.25 2.25 0 0 0 21.75 3H2.25A2.25 2.25 0 0 0 0 5.25v16.5A2.25 2.25 0 0 0 2.25 24h19.5A2.25 2.25 0 0 0 24 21.75V13.5zm-23.25-3h22.5a.75.75 0 0 0 0-1.5H.75a.75.75 0 0 0 0 1.5zM7.5 6V.75a.75.75 0 0 0-1.5 0V6a.75.75 0 0 0 1.5 0zM18 6V.75a.75.75 0 0 0-1.5 0V6A.75.75 0 0 0 18 6zM5.095 14.03a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zm.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5zm-.53 6.53a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zm.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5zm5.845-3.97a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zm.53-1.28A1.125 1.125 0 1 0 12 15a1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5zm-.53 6.53a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zM12 18a1.125 1.125 0 1 0 0 2.25A1.125 1.125 0 0 0 12 18a.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5zm5.845-3.97a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zm.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5zm-.53 6.53a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06zm.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5z"></path></svg>
             </span>
             <span className="header-option-text">{defaultText ? `Ngày nhận phòng - Ngày trả phòng` : `${format(date[0].startDate, "EEE, dd/MM/yyyy", { locale: vi })} - ${format(date[0].endDate, "EEE, dd/MM/yyyy", { locale: vi })}`}</span>
           </div>
@@ -114,48 +113,53 @@ function SearchBar({ setShowRooms }) {
           onChange={handleDateChange}
           moveRangeOnFirstSelection={false}
           ranges={date}
+          className="date"
           minDate={new Date()}
-        />}
+        />
+        }
       </div>
-      <div className="header-search-item" ref={componentRef2} onClick={() => setOpenOptions(!openOptions)}>
-        <div className="header-search-text">
-          <span className="header-icon"><FontAwesomeIcon icon={faChevronDown} /></span>
-          <span className="header-option-text">{`${options.adult} người lớn - ${options.children} trẻ em - ${options.room} phòng`}</span>
+      <div className="header-search-item-box" ref={componentRef2}>
+        <div className="header-search-item" onClick={() => setOpenOptions(!openOptions)}>
+          <div className="header-search-text">
+            <span className="header-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.5 6a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0zM18 6A6 6 0 1 0 6 6a6 6 0 0 0 12 0zM3 23.25a9 9 0 1 1 18 0 .75.75 0 0 0 1.5 0c0-5.799-4.701-10.5-10.5-10.5S1.5 17.451 1.5 23.25a.75.75 0 0 0 1.5 0z"></path></svg>
+            </span>
+            <span className="header-option-text">{`${options.adult} người lớn - ${options.children} trẻ em - ${options.room} phòng`}</span>
+          </div>
+          <div className="option-icon-down">
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
         </div>
-        {openOptions && (
-          <div className="options">
-            <div className="option-item">
-              <span className="option-text">Người lớn</span>
-              <div className="option-counter">
-                <button disabled={options.adult <= 1} className="option-button" onClick={(e) => handleOption(e, 'adult', 'd')}>-</button>
-                <span className="option-number">{options.adult}</span>
-                <button className="option-button" onClick={(e) => handleOption(e, 'adult', 'i')}>+</button>
-              </div>
-            </div>
-            <div className="option-item">
-              <span className="option-text">Trẻ em</span>
-              <div className="option-counter">
-                <button disabled={options.children <= 0} className="option-button" onClick={(e) => handleOption(e, 'children', 'd')}>-</button>
-                <span className="option-number">{options.children}</span>
-                <button className="option-button" onClick={(e) => handleOption(e, 'children', 'i')}>+</button>
-              </div>
-            </div>
-            <div className="option-item">
-              <span className="option-text">Phòng</span>
-              <div className="option-counter">
-                <button disabled={options.room <= 1} className="option-button" onClick={(e) => handleOption(e, 'room', 'd')}>-</button>
-                <span className="option-number">{options.room}</span>
-                <button className="option-button" onClick={(e) => handleOption(e, 'room', 'i')}>+</button>
-              </div>
+        {openOptions && <div className="options">
+          <div className="option-item">
+            <span className="option-text">Người lớn</span>
+            <div className="option-counter">
+              <button disabled={options.adult <= 1} className="option-counter-btn" onClick={(event) => handleOption(event, "adult", "d")}>-</button>
+              <span className="option-counter-number">{options.adult}</span>
+              <button className="option-counter-btn" onClick={(event) => handleOption(event, "adult", "i")}>+</button>
             </div>
           </div>
-        )}
+          <div className="option-item">
+            <span className="option-text">Trẻ em</span>
+            <div className="option-counter">
+              <button disabled={options.children <= 0} className="option-counter-btn" onClick={(event) => handleOption(event, "children", "d")}>-</button>
+              <span className="option-counter-number">{options.children}</span>
+              <button className="option-counter-btn" onClick={(event) => handleOption(event, "children", "i")}>+</button>
+            </div>
+          </div>
+          <div className="option-item">
+            <span className="option-text">Phòng</span>
+            <div className="option-counter">
+              <button disabled={options.room <= 1} className="option-counter-btn" onClick={(event) => handleOption(event, "room", "d")}>-</button>
+              <span className="option-counter-number">{options.room}</span>
+              <button className="option-counter-btn" onClick={(event) => handleOption(event, "room", "i")}>+</button>
+            </div>
+          </div>
+          <button className="option-end" onClick={() => setOpenOptions(!openOptions)}>Xong</button>
+        </div>}
       </div>
-      <div className="header-search-item">
-        <button className="header-button">Tìm kiếm</button>
-      </div>
+      <button className="header-btn">Tìm kiếm</button>
     </form>
-  );
+  )
 }
-
 export default SearchBar;
--- src/customer/pages/reservation/ReservationPage.jsx
+++ src/customer/pages/reservation/ReservationPage.jsx
@@ -16,11 +16,11 @@ import LoadingSpinner from '../../../components/loading-spinner/LoadingSpinner';
 const GoodToKnow = () => {
   return (
     <div className="Good">
-      <h4>Good to know</h4>
-      <div className='no-credit'>
-        <p><FontAwesomeIcon icon={faCheckCircle} /> No credit card needed!</p>
-        <p><FontAwesomeIcon icon={faCheckCircle} /> Stay flexible: Cancel for free at any time.</p>
-        <p><FontAwesomeIcon icon={faCheckCircle} /> Pay when you stay.</p>
+      <h4> Good to know </h4>
+      <div className='no credit'>
+        <p><FontAwesomeIcon icon={faCheckCircle} /> No credit card needed! </p>
+        <p><FontAwesomeIcon icon={faCheckCircle} /> Stay flexible: You can cancel for free at any time, so lock in   this great price today .</p>
+        <p><FontAwesomeIcon icon={faCheckCircle} /> No payment needed today. You'll pay when you stay. </p>
       </div>
     </div>
   );
@@ -29,6 +29,8 @@ const GoodToKnow = () => {
 const SecurePage = ({ hotelId, location }) => {
   const user = JSON.parse(localStorage.getItem("user"));
   const [loading, setLoading] = useState(false);
+
+  // const formik = useFormikContext();
   const navigate = useNavigate();
   const searchParams = new URLSearchParams(location.search);
 
@@ -48,13 +50,12 @@ const SecurePage = ({ hotelId, location }) => {
     try {
       await api.post(`/bookings/hotels/${hotelId}`, requestData);
       setLoading(false);
-      alert("Booking details have been sent.");
+      alert("Thông tin đặt phòng đã được gửi đi. Mọi thông tin về thông tin đặt phòng sẽ được gửi về email ngay khi khách sạn xác nhận. Vui lòng thường xuyên kiểm tra email của bạn!");
       navigate("/");
     } catch (err) {
       console.log(err);
     }
   };
-
   const initialValues = {
     firstname: '',
     lastname: '',
@@ -64,26 +65,36 @@ const SecurePage = ({ hotelId, location }) => {
 
   const validate = (values) => {
     const errors = {};
-    if (!values.firstname) errors.firstname = 'First name is required';
-    if (!values.lastname) errors.lastname = 'Last name is required';
+    if (!values.firstname) {
+      errors.firstname = 'First name is required';
+    }
+
+    if (!values.lastname) {
+      errors.lastname = 'Last name is required';
+    }
+
     if (!values.telephone) {
       errors.telephone = 'Telephone is required';
     } else if (!/^\d+$/.test(values.telephone)) {
       errors.telephone = 'Invalid telephone number';
     }
+
     if (!values.email) {
       errors.email = 'Email is required';
     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
       errors.email = 'Invalid email address';
     }
+
     return errors;
   };
+
+  const cardTypes = ['Visa', 'MasterCard', 'American Express', 'Discover'];
 
   return (
     <div>
       {loading && <LoadingSpinner />}
       <div className='dad'>
-        <div className='Login-template'>
+        <div className='Login template'>
           <div className='form_container p-9 rounded bg-white'>
             <Formik
               initialValues={initialValues}
@@ -92,24 +103,92 @@ const SecurePage = ({ hotelId, location }) => {
             >
               <Form className='form-secure'>
                 <div className='mb-2 d-grid'>
-                  <div className='ad1 mb-2'>
-                    <h3>Enter your details</h3>
-                    <div className='name form-group d-flex w-100 mb-3'>
-                      <div className='m-right' style={{ width: '349px' }}>
-                        <label htmlFor='firstname'>First Name <span className="required text-danger">*</span></label>
-                        <Field id='firstname' type="text" name="firstname" placeholder='Enter first name' className='form-control' />
-                        <ErrorMessage name="firstname" component="span" style={{ color: 'red' }} />
+                  <div className='ad1 mb-2' >
+                    <h3 className='text-right'>Enter your details</h3>
+                    <div className=' name form-group d-flex w-100 mb-3 '>
+                      <div className=' m-right' style={{ width: '349px' }}>
+                        <label htmlFor='firstname' className='form-label' style={{ fontWeight: 'bold' }}>
+                          First Name <span className="required text-danger">*</span>
+                        </label>
+                        <Field
+                          id='firstname'
+                          type="text"
+                          name="firstname"
+                          placeholder='Enter first name'
+                          className='form-control'
+                        />
+                        <ErrorMessage name="firstname" component="span" className='form-message' style={{ color: 'red' }} />
                       </div>
-                      <div className='m-left' style={{ marginLeft: '2rem', width: '315px' }}>
-                        <label htmlFor='lastname'>Last Name <span className="required text-danger">*</span></label>
-                        <Field id="lastname" type="text" name="lastname" placeholder='Enter last name' className='form-control' />
-                        <ErrorMessage name="lastname" component="span" style={{ color: 'red' }} />
+                      <div className='m-lef ' style={{ marginLeft: '2rem', width: '315px' }}>
+                        <label htmlFor='lastname' className='form-label' style={{ fontWeight: 'bold' }}>
+                          Last Name <span className="required text-danger">*</span>
+                        </label>
+                        <Field
+                          id="lastname"
+                          type="text"
+                          name="lastname"
+                          placeholder='Enter last name'
+                          className='form-control'
+                        />
+                        <ErrorMessage name="lastname" component="span" className='form-message' style={{ color: 'red' }} />
+                      </div>
+                    </div>
+
+                    <div className='mb-3 w-50'>
+                      <label htmlFor='telephone' className='form-label ' style={{ fontWeight: 'bold' }}>
+                        Telephone <span className="required text-danger">*</span>
+                      </label>
+                      <Field
+                        id="telephone"
+                        type="text"
+                        name="telephone"
+                        placeholder='Enter telephone'
+                        className='form-control'
+                      />
+                      <ErrorMessage name="telephone" component="span" className='form-message' style={{ color: 'red' }} />
+                    </div>
+                    <div className='mb-3 w-50'>
+                      <label htmlFor='email' className='form-label' style={{ fontWeight: 'bold' }}>
+                        Email <span className="required text-danger">*</span>
+                      </label>
+                      <Field
+                        id="email"
+                        type="email"
+                        name="email"
+                        readOnly={user !== null}
+                        placeholder='Enter email'
+                        className='form-control'
+                      />
+                      <ErrorMessage name="email" component="span" className='form-message' style={{ color: 'red' }} />
+                    </div>
+                    <div className="booking-for-section mt-3">
+                      <p className="mb-2" style={{ fontWeight: 'bold' }}>Who are you booking for?</p>
+                      <div className="form-check mb-2">
+                        <input type="radio" id="mainGuest" name="bookingFor" className="form-check-input" />
+                        <label htmlFor="mainGuest" className="form-check-label">I'm the main guest</label>
+                      </div>
+                      <div className="form-check mb-2">
+                        <input type="radio" id="someoneElse" name="bookingFor" className="form-check-input" />
+                        <label htmlFor="someoneElse" className="form-check-label">I'm booking for someone else</label>
+                      </div>
+                    </div>
+                    <div className="traveling-for-work mt-3">
+                      <p className="mb-2" style={{ fontWeight: 'bold' }}>Are you traveling for work?</p>
+                      <div className="form-check form-check-inline mb-2">
+                        <input type="radio" id="yes" name="travelingForWork" className="form-check-input" />
+                        <label htmlFor="Ayes" className="form-check-label">Yes</label>
+                      </div>
+                      <div className="form-check form-check-inline">
+                        <input type="radio" id="no" name="travelingForWork" className="form-check-input" />
+                        <label htmlFor="Ano" className="form-check-label">No</label>
                       </div>
                     </div>
                   </div>
                   <GoodToKnow />
-                  <div className="mt-3">
-                    <button type="submit" className="btn btn-primary">Complete Booking</button>
+                  <div className=" mt-3">
+                    <button type="submit" className="btn btn-primary">
+                      Complete Booking
+                    </button>
                   </div>
                 </div>
               </Form>
@@ -117,13 +196,16 @@ const SecurePage = ({ hotelId, location }) => {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
+
+
 
 const ReservationPage = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
+
   const checkInDate = new Date(queryParams.get('checkIn'));
   const checkOutDate = new Date(queryParams.get('checkOut'));
   const state = location.state;
@@ -140,51 +222,83 @@ const ReservationPage = () => {
       }
     }
     loadHotelData();
-  }, []);
+  }, [])
+
 
   return (
     <div>
       <Navbar />
-      <div className="content-container">
-        <div className="reservation-container">
+      <div className="content-container align-items-center">
+        <div className="reservation-container" >
           <div className='check'>
             <div className="hotel-information">
               <div className="hotel-info-border">
                 <h5>{hotel.name}</h5>
-                <p>Address: {hotel.address}</p>
-                <div> Rating:
+                <p className=''>Địa chỉ: {hotel.address}</p>
+                <div className='d-flex'  > Rating:
                   <div className="siRating">
                     <button>{hotel.rating?.toFixed(1)}</button>
                   </div>
                 </div>
                 <div className="amenities">
-                  <div className="wifi"><FontAwesomeIcon icon={faWifi} /> <span> Free WiFi</span></div>
-                  <div className="shuttle"><FontAwesomeIcon icon={faShuttleVan} /> <span> Shuttle Service</span></div>
+                  <div className="wifi">
+                    <FontAwesomeIcon icon={faWifi} />
+                    <span> Free WiFi</span>
+                  </div>
+                  <div className="shuttle">
+                    <FontAwesomeIcon icon={faShuttleVan} />
+                    <span> Shuttle Service</span>
+                  </div>
+                </div>
+                <div className="parking">
+                  <FontAwesomeIcon icon={faParking} />
+                  <span> Parking</span>
                 </div>
               </div>
             </div>
 
-            <div className="hotel-detail-border">
+            <div className="hotel-detail-border" >
               <h5>Your booking details</h5>
-              <p>Check-in: <span>{format(checkInDate, "EEE, dd/MM/yyyy", { locale: vi })}</span></p>
-              <p>Check-out: <span>{format(checkOutDate, "EEE, dd/MM/yyyy", { locale: vi })}</span></p>
-              <p>Total length of stay: <span>{state?.stayLength} nights</span></p>
-              <span className="text-success">Change your selection</span>
+              <p style={{ fontWeight: 'bold' }}> Check-in: <span className='check-in'>{format(checkInDate, "EEE, dd/MM/yyyy", { locale: vi })}</span> </p>
+              <p style={{ fontWeight: 'bold' }}> Check-out: <span className='check-out'> {format(checkOutDate, "EEE, dd/MM/yyyy", { locale: vi })} </span></p>
+              <p style={{ fontWeight: 'bold' }}> Total length of stay: <span className='totalStays'> {state?.stayLength} đêm </span> </p>
+              <span className="text-success"> Change your selection </span>
             </div>
 
             <div className='total-summary'>
-              <h4>Your price summary (VND)</h4>
-              <div className='p-3 mb-3' style={{ backgroundColor: '#ADD8E6' }}>
-                <h2>Total: <span>{state?.totalPrice?.toLocaleString('vi-VN')}</span></h2>
+              <h4>Your price summary <span className='unit'> (VND)</span></h4>
+
+              <div className=' p-3 mb-3' style={{ backgroundColor: '#ADD8E6' }}>
+                <h2 className='tp'>
+                  Total: <span className="total-price">{state?.totalPrice.toLocaleString('vi-VN')}</span>
+                </h2>
+              </div>
+
+              <div className="priceInfor bg-light p-3">
+                <h5>Price Information</h5>
+                <p>
+                  <FontAwesomeIcon icon={faMoneyBill} className="mr-5" />
+                  Include VND <span className='vat'>{Math.round(state?.totalPrice / 11).toLocaleString('vi-VN')}</span> in taxes <br /> and charges
+                </p>
+                <p>
+                  10% VAT <span className='tax'>VND {Math.round(state?.totalPrice / 11).toLocaleString('vi-VN')}</span>
+                </p>
               </div>
             </div>
 
+            <div className="Payment">
+              <h5> Your payment schedule </h5>
+              <p> You 'll be charge a payment of the total <br /> price at any time </p>
+            </div>
+          </div>
+          <div>
             <SecurePage hotelId={hotelId} location={location} />
+            <div style={{ height: '200px' }}></div>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default ReservationPage;
+

68066200f01197bc41b53bb5c231a7da4b12f3e9
Sakary03 <107931611+Sakary03@users.noreply.github.com>
2024-11-09 20:25:58 +0700

    Complete hotel-dashboard/chart compt

--- src/hotel-dashboard/components/chart/chart.jsx
+++ src/hotel-dashboard/components/chart/chart.jsx
@@ -22,7 +22,38 @@ const data = [
     { name: "November", Total: 1400 },
     { name: "December", Total: 1900 }
 ];
-
-
+const Chart = ({ aspect, title }) => {
+    return (
+        <div className="p-4 shadow-lg rounded-lg bg-white">
+            <div className="text-lg font-semibold mb-4">{title}</div>
+            <ResponsiveContainer width="100%" aspect={aspect}>
+                <AreaChart
+                    width={730}
+                    height={250}
+                    data={data}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+                >
+                    <defs>
+                        <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="name" stroke="#8884d8" />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Area
+                        type="monotone"
+                        dataKey="Total"
+                        stroke="#8884d8"
+                        fillOpacity={1}
+                        fill="url(#total)"
+                    />
+                </AreaChart>
+            </ResponsiveContainer>
+        </div>
+    );
+};
 
 export default Chart;
+

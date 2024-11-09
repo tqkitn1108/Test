65bca4ad312de332108717b73ce825b9a989476d
Sakary03 <107931611+Sakary03@users.noreply.github.com>
2024-11-09 20:12:47 +0700

    HuyKhoa-Phase2

---
+++ src/hotel-dashboard/components/chart/chart.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+    AreaChart,
+    Area,
+    XAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+} from "recharts";
+
+const data = [
+    { name: "January", Total: 1200 },
+    { name: "February", Total: 2100 },
+    { name: "March", Total: 800 },
+    { name: "April", Total: 1600 },
+    { name: "May", Total: 900 },
+    { name: "June", Total: 1700 },
+    { name: "July", Total: 1500 },
+    { name: "August", Total: 1300 },
+    { name: "September", Total: 1800 },
+    { name: "October", Total: 2000 },
+    { name: "November", Total: 1400 },
+    { name: "December", Total: 1900 }
+];
+
+
+
+export default Chart;
